98e93b4dd3cc5bcaa9291a0ca0c387ca581a0232
Eugenio <emaildeeugenio@gmail.com>
2023-04-21 12:35:32 -0600

    Co-authored-by: Xavier Castro <xavier-castro@users.noreply.github.com>

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -3,5 +3,5 @@ VIDEO_ID=$1
 
 [ -z "$VIDEO_ID" ] && echo "ERROR: No video ID specified" && exit 1
 
-yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" 2>&1
+yt-dlp "https://www.youtube.com/watch?v=QWtI4WQrfC8" --format m4a -o "./tmp/%(id)s.%(ext)s" 2>&1
 
